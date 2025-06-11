afc4e317e5da39ff6045f2a826f8a953a6ddc0b9
DICEsda <yahya24680@gmail.com>
2025-06-11 03:54:53 +0200

    Good progress! got tailwind to work, we got text and a bit of styling

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -11,11 +8,8 @@ export default {
         secondary: "#64ffda",
         tertiary: "#8892b0",
         light: "#ccd6f6",
-      },
-      maxWidth: {
-        '7xl': '80rem',
-      },
+      }
     },
   },
   plugins: [],
-} 
+}
